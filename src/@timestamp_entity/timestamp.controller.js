cae20b6a1e3afb7bcb9bc8dc3484f3e02ae59a18
Bharat Yadav <yadavbharat386@gmail.com>
2025-05-06 13:14:59 +0530

    Add timestamp validation

--- src/@timestamp_entity/timestamp.controller.js
+++ src/@timestamp_entity/timestamp.controller.js
@@ -1,4 +1,5 @@
 const TimestampModel = require("./timestamp.model");
+const VideoModel = require("../@video_entity/video.model");
 const { BadRequestError } = require("../../errors");
 const { StatusCodes } = require("http-status-codes");
 const { default: mongoose } = require("mongoose");
@@ -6,6 +7,8 @@ const { default: mongoose } = require("mongoose");
 exports.addTimeStamp = async (req, res) => {
   const { id: videoId } = req.params;
   const { time, title } = req.body;
+
+  await verifyTimeStamp({ videoId, time });
 
   const timestamp = await TimestampModel.create({
     video: videoId,
@@ -27,26 +30,24 @@ exports.addTimeStamp = async (req, res) => {
 
 exports.updateTimeStamp = async (req, res) => {
   const { id } = req.params;
-  const { time, title } = req.body;
+  let { time, title } = req.body;
+  time = Number(time);
 
-  const timestamp = await TimestampModel.findByIdAndUpdate(
-    id,
-    {
-      time,
-      title,
-    },
-    {
-      runValidators: true,
-      new: true,
-    }
-  );
+  const timestamp = await TimestampModel.findById(id);
   if (!timestamp) {
     throw new BadRequestError("Timestamp updation failed");
   }
 
+  if (timestamp.time !== time) {
+    await verifyTimeStamp({ videoId: timestamp.video, time });
+    timestamp.time = time;
+  }
+  timestamp.title = title;
+  await timestamp.save();
+
   return res.status(StatusCodes.OK).json({
     success: true,
-    message: "Timestamp added successfully",
+    message: "Timestamp updated successfully",
     data: {
       timestamp,
     },
@@ -75,4 +76,44 @@ exports.getVideoTimeStamps = async (videoId) => {
   ]);
 
   return timestamps;
+};
+
+exports.getTimeStamps = async (req, res) => {
+  const { id: videoId } = req.params;
+  const timestamps = await exports.getVideoTimeStamps(videoId);
+  return res.status(StatusCodes.OK).json({
+    success: true,
+    message: "Video timestamps fetched successfully",
+    data: {
+      timestamps,
+    },
+  });
+};
+
+exports.deleteTimestamp = async (req, res) => {
+  const { id } = req.params;
+
+  const timestamp = await TimestampModel.findByIdAndDelete(id);
+  if (!timestamp) {
+    throw new BadRequestError("Timestamp deletion failed");
+  }
+
+  return res.status(StatusCodes.OK).json({
+    success: true,
+    message: "Timestamp deleted successfully",
+  });
+};
+
+const verifyTimeStamp = async ({ videoId, time }) => {
+  const video = await VideoModel.findById(videoId).lean();
+  if (!video) {
+    throw new BadRequestError("Error in accessing video");
+  }
+  const { hours, minutes, seconds } = video.duration;
+  let durationInSec = hours * 3600 + minutes * 60 + seconds;
+  if (time >= durationInSec) {
+    throw new BadRequestError(
+      "Start time cannot be greater than complete video duration"
+    );
+  }
 };
--- src/@timestamp_entity/timestamp.routes.js
+++ src/@timestamp_entity/timestamp.routes.js
@@ -1,11 +1,22 @@
 const express = require("express");
 
-const { addTimeStamp, updateTimeStamp } = require("./timestamp.controller");
+const {
+  addTimeStamp,
+  updateTimeStamp,
+  getTimeStamps,
+  deleteTimestamp,
+} = require("./timestamp.controller");
 const { auth, isAdmin } = require("../../middlewares/authentication");
 
 const router = express.Router();
 
-router.route("/video/:id").post(auth, isAdmin, addTimeStamp);
-router.route("/:id").put(auth, isAdmin, updateTimeStamp);
+router
+  .route("/video/:id")
+  .post(auth, isAdmin, addTimeStamp)
+  .get(auth, isAdmin, getTimeStamps);
+router
+  .route("/:id")
+  .put(auth, isAdmin, updateTimeStamp)
+  .delete(auth, isAdmin, deleteTimestamp);
 
 module.exports = router;
