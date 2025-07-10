90cb7b1efdd484d5e0ab7f6b0b84ac7efbb69461
Bharat Yadav <yadavbharat386@gmail.com>
2025-07-10 18:22:43 +0530

    Update server js

--- config/database.js
+++ config/database.js
@@ -2,11 +2,6 @@ const mongoose = require("mongoose");
 
 mongoose.set("strictQuery", false);
 
-module.exports = () => {
-  mongoose
-    .connect(process.env.MONGO_URL)
-    .then((data) => {
-      console.log(`Database server connected at port: ${data.connection.port}`);
-    })
-    .catch((e) => console.log(e));
+module.exports = (url) => {
+  return mongoose.connect(url);
 };
--- server.js
+++ server.js
@@ -2,18 +2,37 @@ const app = require("./app");
 const connectDatabase = require("./config/database");
 
 process.on("uncaughtException", (err) => {
-  console.log(err);
-  // process.exit(0);
+  console.error("UNCAUGHT EXCEPTION 💥 Shutting down...");
+  console.error(err);
+  process.exit(1); // 1 = error exit
 });
 
-connectDatabase();
+const startServer = async () => {
+  try {
+    await connectDatabase(process.env.MONGO_URL);
+    console.log("Connected to database successfully.");
 
-const port = process.env.PORT || 4000;
-const server = app.listen(port, () => {
-  console.log("app is listening on ", port);
-});
+    const PORT = process.env.PORT || 4000;
+    server = app.listen(PORT, () => {
+      console.log(`Server started at http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error("Database connectivity error:", error);
+    process.exit(1); // exit on failure to start server
+  }
+};
+
+startServer();
 
 process.on("unhandledRejection", (err) => {
-  console.log(err);
-  // server.close(() => process.exit(0));
+  console.error("UNHANDLED REJECTION 💥 Shutting down...");
+  console.error(err);
+
+  if (server) {
+    server.close(() => {
+      process.exit(1); // exit after closing server
+    });
+  } else {
+    process.exit(1);
+  }
 });
