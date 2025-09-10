843a639c175ea69db451bfb69a3e313663e1da01
Bharat Yadav <yadavbharat386@gmail.com>
2025-09-10 11:37:49 +0530

    Dummy push

--- utils/sendSMS.js
+++ utils/sendSMS.js
@@ -1,6 +1,6 @@
 const twilio = require("twilio");
 
-// Twilio credentials
+//Remove: Twilio credentials
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const from = process.env.TWILIO_PHONE_NUMBER;
